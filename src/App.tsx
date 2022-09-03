aede93c66326f9c84b6da2be9abf3556068deb49
Thien <thien5596@gmail.com>
2022-09-03 08:49:32 +0300

    Change page name to "Manga"

--- src/App.tsx
+++ src/App.tsx
@@ -6,16 +6,16 @@ import {
 } from 'react-router-dom'
 import RootWrapper from 'components/RootWrapper'
 import Home from 'pages/Home'
-import About from 'pages/About'
+import Manga from 'pages/Manga'
 
 export default function App() {
 	return (
 		<Router>
 			<RootWrapper>
 				<Routes>
-					<Route path='/' element={<Navigate replace to='/Home' />} />
+					<Route path='/' element={<Navigate replace to='/Manga' />} />
 					<Route path='/Home' element={<Home />} />
-					<Route path='/About' element={<About />} />
+					<Route path='/Manga' element={<Manga />} />
 				</Routes>
 			</RootWrapper>
 		</Router>
