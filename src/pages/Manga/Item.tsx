ebeb1e26c8493f640a77e9a286617d62a2ceaa8e
Thien <thien5596@gmail.com>
2022-09-09 02:33:03 +0300

    Add useLongPress hook

--- src/pages/Manga/Item.tsx
+++ src/pages/Manga/Item.tsx
@@ -1,4 +1,6 @@
-import { Box, ListItemText } from '@mui/material'
+import { Box, ListItemText, Menu, MenuItem } from '@mui/material'
+import useLongPress from 'hooks/useLongPress'
+import { useState } from 'react'
 
 const Item = ({
 	id,
@@ -11,8 +13,16 @@ const Item = ({
 	name: string
 	chapter: string
 }) => {
+	const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null)
+	const open = Boolean(anchorEl)
+
+	const onLongPress = () => {
+		console.log('longpress is triggered')
+	}
+
 	return (
 		<Box
+			{...useLongPress(onLongPress)}
 			sx={{
 				display: 'flex',
 				gap: 0.5,
@@ -27,6 +37,11 @@ const Item = ({
 			<ListItemText primary={counter} sx={{ flex: 1, textAlign: 'center' }} />
 			<ListItemText primary={name} sx={{ flex: 8, pl: 1 }} />
 			<ListItemText primary={chapter} sx={{ flex: 2, textAlign: 'center' }} />
+			<Menu anchorEl={anchorEl} open={open} onClose={() => setAnchorEl(null)}>
+				<MenuItem onClick={() => setAnchorEl(null)}>Profile</MenuItem>
+				<MenuItem onClick={() => setAnchorEl(null)}>My account</MenuItem>
+				<MenuItem onClick={() => setAnchorEl(null)}>Logout</MenuItem>
+			</Menu>
 		</Box>
 	)
 }
