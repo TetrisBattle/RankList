9934192eed479edd1ad0b8a0431f59ff4187a148
Thien <thien5596@gmail.com>
2022-09-12 20:05:43 +0300

    Create new firebase project and hide firebase config for security reasons

--- src/stores/UserStore.tsx
+++ src/stores/UserStore.tsx
@@ -6,10 +6,14 @@ import {
 	signOut,
 	User,
 } from 'firebase/auth'
+import { collection, doc, getDocs, getFirestore, setDoc } from 'firebase/firestore'
+import firebaseApp from 'firebaseApp'
 
 export default class UserStore {
+	private _db = getFirestore(firebaseApp)
 	private _googleAuthProvider = new GoogleAuthProvider()
 	private _currentUser: User | null | undefined
+	private _dbPath: string | null = null
 
 	constructor() {
 		makeAutoObservable(this)
@@ -23,11 +27,38 @@ export default class UserStore {
 		this._currentUser = value
 	}
 
+	get dbPath() {
+		return this._dbPath
+	}
+
+	set dbPath(value) {
+		this._dbPath = value
+	}
+
 	login() {
 		signInWithRedirect(getAuth(), this._googleAuthProvider)
 	}
 
 	logout() {
 		signOut(getAuth())
+	}
+
+	async checkIfNewUser(user: User) {
+		let newUser = true
+		const colRef = collection(this._db, `users`)
+		const docsSnap = await getDocs(colRef)
+		docsSnap.docs.forEach((doc) => {
+			if (doc.id === user.email) {
+				newUser = false
+				return
+			}
+		})
+
+		if (newUser && user.email) {
+			const userRef = doc(this._db, `users`, user.email)
+			await setDoc(userRef, {
+				displayName: user.displayName,
+			})
+		}
 	}
 }
