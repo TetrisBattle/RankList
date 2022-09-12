ae4642aeb5dda6274aba8cdc68a35c8f6f9becbb
Thien <thien5596@gmail.com>
2022-09-12 20:51:26 +0300

    Remove unnecessary files and restructure project

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,64 @@
-import {
-	BrowserRouter as Router,
-	Routes,
-	Route,
-	Navigate,
-} from 'react-router-dom'
-import RootWrapper from 'components/RootWrapper'
-// import Login from 'pages/Login'
-import Manga from 'pages/Manga'
+import { observer } from 'mobx-react-lite'
+import { Backdrop, Box, Button, CircularProgress } from '@mui/material'
+import { useStoreContext } from 'stores/StoreContext'
+import TopBar from 'components/TopBar'
+import MangaList from 'components/MangaList'
+import MangaDialog from 'components/MangaDialog'
+import { getAuth, onAuthStateChanged } from 'firebase/auth'
+import { useEffect } from 'react'
 
-export default function App() {
+const App = () => {
+	const { appStore, userStore, mangaStore } = useStoreContext()
+
+	useEffect(() => {
+		const unsubscribe = onAuthStateChanged(getAuth(), (user) => {
+			userStore.currentUser = user
+			mangaStore.mangaPath = user ? `users/${user.uid}/lists/manga` : null
+		})
+		return unsubscribe
+	}, [userStore, mangaStore])
+
 	return (
-		<Router>
-			<RootWrapper>
-				<Routes>
-					<Route path='/' element={<Navigate replace to={'/manga'} />} />
-					{/* <Route path='/Login' element={<Login />} /> */}
-					<Route path='/Manga' element={<Manga />} />
-				</Routes>
-			</RootWrapper>
-		</Router>
+		<Box
+			sx={(theme) => ({
+				bgcolor: theme.palette.background.default,
+				minHeight: '100vh',
+				color: theme.palette.text.primary,
+			})}
+		>
+			<Box
+				sx={{
+					maxWidth: (theme) => theme.breakpoints.values.md,
+					marginInline: 'auto',
+				}}
+			>
+				<TopBar />
+				{userStore.currentUser === null && (
+					<Button
+						onClick={() => userStore.login()}
+						sx={{
+							display: 'block',
+							mt: 2,
+							mx: 'auto',
+							fontWeight: 600,
+						}}
+					>
+						Log in with Google
+					</Button>
+				)}
+				<MangaList />
+				{mangaStore.isLoading && (
+					<Box sx={{ mt: 2, display: 'flex', justifyContent: 'center' }}>
+						<CircularProgress />
+					</Box>
+				)}
+			</Box>
+			<MangaDialog />
+			<Backdrop open={appStore.isLoading} sx={{ zIndex: 9999 }}>
+				<CircularProgress />
+			</Backdrop>
+		</Box>
 	)
 }
+
+export default observer(App)
--- src/material-ui/MuiThemeProvider.tsx
+++ src/material-ui/MuiThemeProvider.tsx
@@ -2,17 +2,15 @@ import { observer } from 'mobx-react-lite'
 import { ThemeProvider } from '@mui/material/styles'
 import MuiTheme from './MuiTheme'
 import { useStoreContext } from 'stores/StoreContext'
-import { ReactChildren } from 'interfaces/react'
+import React from 'react'
 
-const MuiThemeProvider = ({ children }: ReactChildren) => {
-	const { appStore: { isDarkTheme } } = useStoreContext()
+const MuiThemeProvider = ({ children }: { children: React.ReactNode }) => {
+	const {
+		appStore: { isDarkTheme },
+	} = useStoreContext()
 	const theme = MuiTheme(isDarkTheme)
 
-	return (
-		<ThemeProvider theme={theme}>
-			{children}
-		</ThemeProvider>
-	)
+	return <ThemeProvider theme={theme}>{children}</ThemeProvider>
 }
 
 export default observer(MuiThemeProvider)
